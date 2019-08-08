2c389b11f5efbddd87f66470bb13a4e612ec1e42
Marco Angel <angel2367716@gmail.com>
2019-08-08 11:35:31 -0700

    changed stuff on server

--- 13-StarWars-6/server6.js
+++ 13-StarWars-6/server6.js
@@ -44,7 +44,7 @@ const characters = [
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
   // res.send("Welcome to the Star Wars Page!")
-  res.json(path.join(__dirname, "view.html"));
+  res.sendFile(path.join(__dirname, "view.html"));
 });
 
 // Displays all characters
